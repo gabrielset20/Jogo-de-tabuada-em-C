c875a4cddacde15f73a11e2932379552d4512848
gabrielset20 <114269079+gabrielset20@users.noreply.github.com>
2023-04-01 18:38:31 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <locale.h>
 /*
-    Exerício: fazer um código para o público infantil, com o objetivo de ensinar operações de tabuada por meio de uma dinâmica de jogo.
+    ExercÃ­cio: fazer um cÃ³digo para o pÃºblico infantil, com o objetivo de ensinar operaÃ§Ãµes de tabuada por meio de uma dinÃ¢mica de jogo.
 */
 void regras_A();
 void titulo_T();
@@ -26,7 +26,7 @@ int main()
     system("cls");
     titulo_T();
     printf("\n\t\t    Vamos nos preparar para jogar \\(^ w ^ *)\n\t\t\t      %s e %s!\n\n", player1, player2);
-    printf("\n\t\t      Escolham uma operação para iniciar!\n\t\t          1.[+]  2.[-]  3.[x]  4.[/]\n\t\t");
+    printf("\n\t\t      Escolham uma operaÃ§Ã£o para iniciar!\n\t\t          1.[+]  2.[-]  3.[x]  4.[/]\n\t\t");
     scanf(" %d", &op);
     switch (op){
         case 1:
@@ -42,18 +42,18 @@ int main()
             scanf(" %d", &valor2);
             system("cls");
             titulo_T();
-            printf("\n\t\t%s, resolva a operação abaixo:\n", player2);
+            printf("\n\t\t%s, resolva a operaÃ§Ã£o abaixo:\n", player2);
             printf("\n\t\t %d + %d = ", valor1, valor2);
             scanf(" %d", &resposta);
             resultado = valor1 + valor2;
             if (resposta == resultado){
-                printf("\n\t\tPARABÉNS, %s \(0o0)/! Resposta correta! Próxima rodada: \n\t\t", player2);
+                printf("\n\t\tPARABÃ‰NS, %s \(0o0)/! Resposta correta! PrÃ³xima rodada: \n\t\t", player2);
                 score_player2++;
                 system("pause");
             }
                 else{
-                    printf("\n\t\tERRADO, %s! Resposta incorreta! Próxima rodada: \n\t\t", player2);
-                    printf("\n\t\tA resposta é: %d.\n\t\t", resultado);
+                    printf("\n\t\tERRADO, %s! Resposta incorreta! PrÃ³xima rodada: \n\t\t", player2);
+                    printf("\n\t\tA resposta Ã©: %d.\n\t\t", resultado);
                     system("pause");
                 }
             system("cls");
@@ -66,23 +66,23 @@ int main()
             scanf(" %d", &valor2);
             system("cls");
             titulo_T();
-            printf("\n\t\t%s, resolva a operação abaixo:\n", player1);
+            printf("\n\t\t%s, resolva a operaÃ§Ã£o abaixo:\n", player1);
             printf("\n\t\t %d + %d = ", valor1, valor2);
             scanf(" %d", &resposta);
             resultado = valor1 + valor2;
             if (resposta == resultado){
-                printf("\n\t\tPARABÉNS, %s \(0o0)/! Resposta correta! Próxima rodada: \n\t\t", player1);
+                printf("\n\t\tPARABÃ‰NS, %s \(0o0)/! Resposta correta! PrÃ³xima rodada: \n\t\t", player1);
                 score_player1++;
                 system("pause");
             }
                 else{
-                    printf("\n\t\tERRADO, %s! Resposta incorreta! Próxima rodada: \n\t\t", player1);
-                    printf("\n\t\tA resposta é: %d.\n\t\t", resultado);
+                    printf("\n\t\tERRADO, %s! Resposta incorreta! PrÃ³xima rodada: \n\t\t", player1);
+                    printf("\n\t\tA resposta Ã©: %d.\n\t\t", resultado);
                     system("pause");
                 }
             system("cls");
             titulo_T();
-            printf("\n\n\t\tApertem 1 para continuar na adição? \(>,<)/ \n\t\tApertem 0 para mudar de operação! -.-)/\n\t\tApertem 2 para sair do jogo e ver o score final! (^W^)/\n\t\t");
+            printf("\n\n\t\tApertem 1 para continuar na adiÃ§Ã£o? \(>,<)/ \n\t\tApertem 0 para mudar de operaÃ§Ã£o! -.-)/\n\t\tApertem 2 para sair do jogo e ver o score final! (^W^)/\n\t\t");
             setbuf(stdin,NULL);
             scanf(" %d", &op2);
             }
@@ -103,18 +103,18 @@ int main()
             scanf(" %d", &valor2);
             system("cls");
             titulo_T();
-            printf("\n\t\t%s, resolva a operação abaixo:\n", player2);
+            printf("\n\t\t%s, resolva a operaÃ§Ã£o abaixo:\n", player2);
             printf("\n\t\t %d - %d = ", valor1, valor2);
             scanf(" %d", &resposta);
             resultado = valor1 - valor2;
             if (resposta == resultado){
-                printf("\n\t\tPARABÉNS, %s \(0o0)/! Resposta correta! Próxima rodada: \n\t\t", player2);
+                printf("\n\t\tPARABÃ‰NS, %s \(0o0)/! Resposta correta! PrÃ³xima rodada: \n\t\t", player2);
                 score_player2++;
                 system("pause");
             }
                 else{
-                    printf("\n\t\tERRADO, %s! Resposta incorreta! Próxima rodada: \n\t\t", player2);
-                    printf("\n\t\tA resposta é: %d.\n\t\t", resultado);
+                    printf("\n\t\tERRADO, %s! Resposta incorreta! PrÃ³xima rodada: \n\t\t", player2);
+                    printf("\n\t\tA resposta Ã©: %d.\n\t\t", resultado);
                     system("pause");
                 }
             system("cls");
@@ -127,23 +127,23 @@ int main()
             scanf(" %d", &valor2);
             system("cls");
             titulo_T();
-            printf("\n\t\t%s, resolva a operação abaixo:\n", player1);
+            printf("\n\t\t%s, resolva a operaÃ§Ã£o abaixo:\n", player1);
             printf("\n\t\t %d - %d = ", valor1, valor2);
             scanf(" %d", &resposta);
             resultado = valor1 - valor2;
             if (resposta == resultado){
-                printf("\n\t\tPARABÉNS, %s \(0o0)/! Resposta correta! Próxima rodada: \n\t\t", player1);
+                printf("\n\t\tPARABÃ‰NS, %s \(0o0)/! Resposta correta! PrÃ³xima rodada: \n\t\t", player1);
                 score_player1++;
                 system("pause");
             }
                 else{
-                    printf("\n\t\tERRADO, %s! Resposta incorreta! Próxima rodada: \n\t\t", player1);
-                    printf("\n\t\tA resposta é: %d.\n\t\t", resultado);
+                    printf("\n\t\tERRADO, %s! Resposta incorreta! PrÃ³xima rodada: \n\t\t", player1);
+                    printf("\n\t\tA resposta Ã©: %d.\n\t\t", resultado);
                     system ("pause");
                 }
             system("cls");
             titulo_T();
-            printf("\n\n\t\tApertem 1 para continuar na subtração? \(>,<)/ \n\t\tApertem 0 para mudar de operação! -.-)/\n\t\tApertem 2 para sair do jogo e ver o score final! (^W^)/\n\t\t");
+            printf("\n\n\t\tApertem 1 para continuar na subtraÃ§Ã£o? \(>,<)/ \n\t\tApertem 0 para mudar de operaÃ§Ã£o! -.-)/\n\t\tApertem 2 para sair do jogo e ver o score final! (^W^)/\n\t\t");
             setbuf(stdin,NULL);
             scanf(" %d", &op2);
             }
@@ -163,18 +163,18 @@ int main()
             scanf(" %d", &valor2);
             system("cls");
             titulo_T();
-            printf("\n\t\t%s, resolva a operação abaixo:\n", player2);
+            printf("\n\t\t%s, resolva a operaÃ§Ã£o abaixo:\n", player2);
             printf("\n\t\t %d x %d = ", valor1, valor2);
             scanf(" %d", &resposta);
             resultado = valor1 * valor2;
             if (resposta == resultado){
-                printf("\n\t\tPARABÉNS, %s \(0o0)/! Resposta correta! Próxima rodada: \n\t\t", player2);
+                printf("\n\t\tPARABÃ‰NS, %s \(0o0)/! Resposta correta! PrÃ³xima rodada: \n\t\t", player2);
                 score_player2++;
                 system("pause");
             }
                 else{
-                    printf("\n\t\tERRADO, %s! Resposta incorreta! Próxima rodada: \n\t\t", player2);
-                    printf("\n\t\tA resposta é: %d.\n\t\t", resultado);
+                    printf("\n\t\tERRADO, %s! Resposta incorreta! PrÃ³xima rodada: \n\t\t", player2);
+                    printf("\n\t\tA resposta Ã©: %d.\n\t\t", resultado);
                     system ("pause");
                 }
             system("cls");
@@ -187,23 +187,23 @@ int main()
             scanf(" %d", &valor2);
             system("cls");
             titulo_T();
-            printf("\n\t\t%s, resolva a operação abaixo:\n", player1);
+            printf("\n\t\t%s, resolva a operaÃ§Ã£o abaixo:\n", player1);
             printf("\n\t\t %d x %d = ", valor1, valor2);
             scanf(" %d", &resposta);
             resultado = valor1 * valor2;
             if (resposta == resultado){
-                printf("\n\t\tPARABÉNS, %s \(0o0)/! Resposta correta! Próxima rodada: \n\t\t", player1);
+                printf("\n\t\tPARABÃ‰NS, %s \(0o0)/! Resposta correta! PrÃ³xima rodada: \n\t\t", player1);
                 score_player1++;
                 system("pause");
             }
                 else{
-                    printf("\n\t\tERRADO, %s! Resposta incorreta! Próxima rodada: \n\t\t", player1);
-                    printf("\n\t\tA resposta é: %d.\n\t\t", resultado);
+                    printf("\n\t\tERRADO, %s! Resposta incorreta! PrÃ³xima rodada: \n\t\t", player1);
+                    printf("\n\t\tA resposta Ã©: %d.\n\t\t", resultado);
                     system ("pause");
                 }
             system("cls");
             titulo_T();
-            printf("\n\n\t\tApertem 1 para continuar na multiplicação? \(>,<)/ \n\t\tApertem 0 para mudar de operação! -.-)/\n\t\tApertem 2 para sair do jogo e ver o score final! (^W^)/\n\t\t");
+            printf("\n\n\t\tApertem 1 para continuar na multiplicaÃ§Ã£o? \(>,<)/ \n\t\tApertem 0 para mudar de operaÃ§Ã£o! -.-)/\n\t\tApertem 2 para sair do jogo e ver o score final! (^W^)/\n\t\t");
             setbuf(stdin,NULL);
             scanf(" %d", &op2);
             }
@@ -223,18 +223,18 @@ int main()
             scanf(" %d", &valor2);
             system("cls");
             titulo_T();
-            printf("\n\t\t%s, resolva a operação abaixo:\n", player2);
+            printf("\n\t\t%s, resolva a operaÃ§Ã£o abaixo:\n", player2);
             printf("\n\t\t %d / %d = ", valor1, valor2);
             scanf(" %d", &resposta);
             resultado = valor1 / valor2;
             if (resposta == resultado){
-                printf("\n\t\tPARABÉNS, %s \(0o0)/! Resposta correta! Próxima rodada: \n\t\t", player2);
+                printf("\n\t\tPARABÃ‰NS, %s \(0o0)/! Resposta correta! PrÃ³xima rodada: \n\t\t", player2);
                 score_player2++;
                 system("pause");
             }
                 else{
-                    printf("\n\t\tERRADO, %s! Resposta incorreta! Próxima rodada: \n\t\t", player2);
-                    printf("\n\t\tA resposta é: %d.\n\t\t", resultado);
+                    printf("\n\t\tERRADO, %s! Resposta incorreta! PrÃ³xima rodada: \n\t\t", player2);
+                    printf("\n\t\tA resposta Ã©: %d.\n\t\t", resultado);
                 }
             system("cls");
             titulo_T();
@@ -246,22 +246,22 @@ int main()
             scanf(" %d", &valor2);
             system("cls");
             titulo_T();
-            printf("\n\t\t%s, resolva a operação abaixo:\n", player1);
+            printf("\n\t\t%s, resolva a operaÃ§Ã£o abaixo:\n", player1);
             printf("\n\t\t %d / %d = ", valor1, valor2);
             scanf(" %d", &resposta);
             resultado = valor1 / valor2;
             if (resposta == resultado){
-                printf("\n\t\tPARABÉNS, %s \(0o0)/! Resposta correta! Próxima rodada: \n\t\t", player1);
+                printf("\n\t\tPARABÃ‰NS, %s \(0o0)/! Resposta correta! PrÃ³xima rodada: \n\t\t", player1);
                 score_player1++;
                 system("pause");
             }
                 else{
-                    printf("\n\t\tERRADO, %s! Resposta incorreta! Próxima rodada: \n\t\t", player1);
-                    printf("\n\t\tA resposta é: %d.\n\t\t", resultado);
+                    printf("\n\t\tERRADO, %s! Resposta incorreta! PrÃ³xima rodada: \n\t\t", player1);
+                    printf("\n\t\tA resposta Ã©: %d.\n\t\t", resultado);
                 }
             system("cls");
             titulo_T();
-            printf("\n\n\t\tApertem 1 para continuar na divisão? \(>,<)/ \n\t\tApertem 0 para mudar de operação! -.-)/\n\t\tApertem 2 para sair do jogo e ver o score final! (^W^)/\n\t\t");
+            printf("\n\n\t\tApertem 1 para continuar na divisÃ£o? \(>,<)/ \n\t\tApertem 0 para mudar de operaÃ§Ã£o! -.-)/\n\t\tApertem 2 para sair do jogo e ver o score final! (^W^)/\n\t\t");
             setbuf(stdin,NULL);
             scanf(" %d", &op2);
             }
@@ -270,7 +270,7 @@ int main()
                     if (op2 == 2) goto gameover;
         break;
         default:
-            printf("\n\t\tOpção inválida! (*-_-) Tente novamente, jogadores.");
+            printf("\n\t\tOpÃ§Ã£o invÃ¡lida! (*-_-) Tente novamente, jogadores.");
             system("pause");
             return (0);
         break;
@@ -278,7 +278,7 @@ int main()
     gameover:
         system("cls");
         titulo_T();
-        printf("\n\t\t            O score final é: \n\t\t");
+        printf("\n\t\t            O score final Ã©: \n\t\t");
         printf("\n\t\t          %s = %d pontos e %s = %d pontos\n\t\t", player1, score_player1, player2, score_player2);
         printf("\n\t\tMuito obrigado por usar esse programa! <('.'<)\n\t\t");
         system("pause");
@@ -286,15 +286,15 @@ int main()
 }
 void titulo_T(){
     printf("\n\t\t  <<<<<<<                            >,<  ");
-    printf("\n\t\t              Jogo de Operações           ");
+    printf("\n\t\t              Jogo de OperaÃ§Ãµes           ");
     printf("\n\t\t  \(*v*)/                          >>>>>>>");
 }
 void regras_A(){
 printf("\n\t\t                  R E G R A S                  \n");
-printf("\n\t\t1. Identifique os jogadores antes de começar\n\t\ta jogar;\n");
-printf("\n\t\t2. Escolha uma das opções 1, 2, 3 ou 4 referente\n\t\ta operação desejada;\n");
-printf("\n\t\t3. O primeiro jogador deverá colocar dois valores\n\t\t(números) para o outro tentar realizar a operação;\n");
-printf("\n\t\t4. Os pontos contabilizados serão mostrados no\n\t\tencerramento do jogo;\n");
+printf("\n\t\t1. Identifique os jogadores antes de comeÃ§ar\n\t\ta jogar;\n");
+printf("\n\t\t2. Escolha uma das opÃ§Ãµes 1, 2, 3 ou 4 referente\n\t\ta operaÃ§Ã£o desejada;\n");
+printf("\n\t\t3. O primeiro jogador deverÃ¡ colocar dois valores\n\t\t(nÃºmeros) para o outro tentar realizar a operaÃ§Ã£o;\n");
+printf("\n\t\t4. Os pontos contabilizados serÃ£o mostrados no\n\t\tencerramento do jogo;\n");
 printf("\n\t\t5. Divirta-se! \n\t\t");
 system("pause");
 }
